a839ed3566f5dae568a16211a6b23f53f6e51aaf
Edo Notes <edoono24@gmail.com>
2017-12-31 01:09:07 +0200

    DB Changes
    Signed-off-by: Edo Notes <edoono24@gmail.com>

--- zerLiDB-t.sql
+++ zerLiDB-t.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `zerli` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `zerli`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: zerli
@@ -341,7 +343,7 @@ CREATE TABLE `user` (
   `ID` int(9) NOT NULL,
   `FirstName` varchar(45) DEFAULT NULL,
   `LastName` varchar(45) DEFAULT NULL,
-  `OnlineStatus` enum('Online','Offline') DEFAULT NULL,
+  `ConnectionStatus` enum('Online','Offline','Blocked') DEFAULT NULL,
   `Premission` enum('Client','StoreManager',' StoreEmpployee','Expert','CostomerServiece') DEFAULT NULL,
   `Phone` varchar(10) DEFAULT NULL,
   `Gender` varchar(10) DEFAULT NULL,
@@ -369,4 +371,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-28  2:35:47
+-- Dump completed on 2017-12-31  1:08:44
