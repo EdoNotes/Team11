07150b3de26dcc95a3df817696b90fe5a4e5027a
matan <19matan@gmail.com>
2018-01-03 00:28:23 +0200

    small change on survey
    Signed-off-by: matan <19matan@gmail.com>

--- Zerli-Team11/src/Gui/SurveyController.java
+++ Zerli-Team11/src/Gui/SurveyController.java
@@ -10,8 +10,31 @@
  **************************************************************************/
 package Gui;
 
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SurveyController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+public class SurveyController implements Initializable
+{
+	
+	@FXML
+	ComboBox cmbSelectAnswer1;
+	@FXML
+	ComboBox cmbSelectAnswer2;
+	
+	ObservableList<String> OneToTen=FXCollections.observableArrayList("1","2","3","4","5","6","7","8","9","10");
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		cmbSelectAnswer1.setItems(OneToTen);
+		cmbSelectAnswer2.setItems(OneToTen);
+	}
 
 }
