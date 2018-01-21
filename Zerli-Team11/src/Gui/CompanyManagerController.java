2e31e1642fc194d4311ad915509c81b4aa21a7f1
NTYOK.MTL.COM\idok <idotehila@gmail.com>
2018-01-21 11:20:34 +0200

    Adding option to choose a specific date include indicate year to get reports.

--- Zerli-Team11/src/Gui/CompanyManagerController.java
+++ Zerli-Team11/src/Gui/CompanyManagerController.java
@@ -53,6 +53,10 @@ public class CompanyManagerController implements Initializable
 	@FXML
 	ComboBox cmbQ2;
 	@FXML
+	ComboBox<String> year1;
+	@FXML
+	ComboBox<String> year2;
+	@FXML
 	Button Breport2;
 	@FXML
 	Button Breport1;
@@ -61,6 +65,7 @@ public class CompanyManagerController implements Initializable
 	ObservableList <String >quarterly = FXCollections.observableArrayList("1","2","3","4");
 	ObservableList<String> ReportsList=FXCollections.observableArrayList("Quarter's Incomes","Quarter's Order","Customers Complaints","Customer Satisfication");
 	ObservableList<String> ShopList=FXCollections.observableArrayList("1","2","3");
+	ObservableList<String> yearlist=FXCollections.observableArrayList("2009","2010","2011","2012","2013","2014","2015","2016","2017","2018");
 	public ClientConsole client;
 	public ChatClient chat;
 	private Msg LogoutMsg=new Msg();
@@ -74,6 +79,10 @@ public class CompanyManagerController implements Initializable
 		cmbQ2.setItems(quarterly);
 		cmbS1.setItems(ShopList);
 		cmbS2.setItems(ShopList);
+		year1.setItems(yearlist);
+		year2.setItems(yearlist);
+
+		
 	}
 	@FXML
 	public void RegisterBtn(ActionEvent event) throws IOException
@@ -96,20 +105,24 @@ public class CompanyManagerController implements Initializable
 		TreeMap<String, String> directory = new TreeMap<String, String>();
 		Msg userToCheck=new Msg(Msg.qSELECTALL,"checkUserExistence");
 		userToCheck.setClassType("report");// create a new msg
-		String[] date= {"'2018-01-01' AND '2018-3-31'", "'2018-4-01' AND '2018-12-06'","'2018-07-01' AND '2018-09-31'", "'2018-10-01' AND '2018-12-31'"}; 
+		
+		int qutere =Integer.parseInt((String)cmbQ1.getValue());  //build the computable string for the chosen quarterly.
+		int monthStart= (qutere*3) -2;
+		int monthEnd= (qutere*3);
+		String qutr="'"+(String)year1.getValue()+"-"+monthStart+"-01' AND '"+(String)year1.getValue()+"-"+monthEnd+"-31'";
+		System.out.println(qutr);
 		String cmd = "";
 		String cmd_count ="SELECT  p.productType, count(*) FROM zerli.order o, zerli.product_in_order pid, zerli.product p where o.Date BETWEEN";
 		String cmd_sum = "SELECT  p.productType, sum(p.price) FROM zerli.order o, zerli.product_in_order pid, zerli.product p where o.Date BETWEEN";
 		String cmd_complain = "SELECT month(assigningDate) as M ,count(*) as count FROM zerli.complaint  WHERE assigningDate BETWEEN";
 		String cmd_survey = "SELECT avg(answer1),avg(answer2),avg(answer3),avg(answer4),avg(answer5), "
 				+ "avg(answer6) FROM zerli.survey_answer where numSurvey = 1;";
-
+			//Chosen the suitable cmd for the request order.
 			if((String)cmbSelectReport1.getValue()=="Quarter's Incomes") cmd=cmd_sum;
 			if((String)cmbSelectReport1.getValue()=="Quarter's Order") cmd=cmd_count;
 			if((String)cmbSelectReport1.getValue()=="Customers Complaints") cmd=cmd_complain; 
 		System.out.println((String)cmbQ1.getValue());
-		int q2 =Integer.parseInt((String)cmbQ1.getValue());
-		cmd += date[q2-1];
+		cmd += qutr;
 		if ((String)cmbSelectReport1.getValue()!="Customers Complaints")
 		{cmd +=" and o.storeID = '" + cmbS1.getValue() +"' and o.orderId=pid.OrderID and pid.productID = p.productID group by p.productType;";}
 		else {cmd += "  group by month(assigningDate)";}
@@ -131,13 +144,63 @@ public class CompanyManagerController implements Initializable
 		Pane root = loader.load(getClass().getResource("report_order.fxml").openStream());
 		report_orderController report=loader.getController();
 		report.setdirectory(directory, (String)cmbSelectReport1.getValue());
-		report.load_dir(directory);
+		report.load_dir(directory);    //load to the next screen the orders details.
 		Scene serverScene = new Scene(root);
 		serverScene.getStylesheets().add(getClass().getResource("report_order.css").toExternalForm());
 		primaryStage.setScene(serverScene);
 		primaryStage.show();
 	}
 
+	public void askReport2(ActionEvent event) throws Exception
+{		//This method makes the second report.
+		TreeMap<String, String> directory = new TreeMap<String, String>();
+		Msg userToCheck=new Msg(Msg.qSELECTALL,"checkUserExistence");
+		userToCheck.setClassType("report");// create a new msg
+		int qutere =Integer.parseInt((String)cmbQ2.getValue());  //build the computable string for the chosen quarterly.
+		int monthStart= (qutere*3) -2;
+		int monthEnd= (qutere*3);
+		String qutr="'"+(String)year1.getValue()+"-"+monthStart+"-01' AND '"+(String)year1.getValue()+"-"+monthEnd+"-31'";
+		System.out.println(qutr);
+		
+		String cmd = "";
+		String cmd_count ="SELECT  p.productType, count(*) FROM zerli.order o, zerli.product_in_order pid, zerli.product p where o.Date BETWEEN";
+		String cmd_sum = "SELECT  p.productType, sum(p.price) FROM zerli.order o, zerli.product_in_order pid, zerli.product p where o.Date BETWEEN";
+		String cmd_complain = "SELECT month(assigningDate) as M ,count(*) as count FROM zerli.complaint  WHERE assigningDate BETWEEN";
+		String cmd_survey = "SELECT avg(answer1),avg(answer2),avg(answer3),avg(answer4),avg(answer5), "
+				+ "avg(answer6) FROM zerli.survey_answer where numSurvey = 1;";
+
+			if((String)cmbSelectReport2.getValue()=="Quarter's Incomes") cmd=cmd_sum;
+			if((String)cmbSelectReport2.getValue()=="Quarter's Order") cmd=cmd_count;
+			if((String)cmbSelectReport2.getValue()=="Customers Complaints") cmd=cmd_complain; 
+		System.out.println((String)cmbQ2.getValue());
+		cmd += qutr;
+		if ((String)cmbSelectReport2.getValue()!="Customers Complaints")
+		{cmd +=" and o.storeID = '" + cmbS2.getValue() +"' and o.orderId=pid.OrderID and pid.productID = p.productID group by p.productType;";}
+		else {cmd += "  group by month(assigningDate)";}
+		System.out.println(cmd);
+		if ((String)cmbSelectReport2.getValue()!="Customer Satisfication")
+		{userToCheck.setQueryQuestion(cmd);}
+		else
+		{userToCheck.setQueryQuestion(cmd_survey);
+		userToCheck.setClassType("survey_report");	
+		}
+		System.out.println("cmd " +userToCheck.getQueryQuestion());
+		client=new ClientConsole(EchoServer.HOST,EchoServer.DEFAULT_PORT);
+		client.accept((Object)userToCheck);
+		directory = (TreeMap<String, String> )client.msg;
+		System.out.println("good 2" +directory);
+		
+		Stage primaryStage=new Stage();
+		FXMLLoader loader = new FXMLLoader();
+		Pane root = loader.load(getClass().getResource("report_order.fxml").openStream());
+		report_orderController report=loader.getController();
+		report.setdirectory(directory, (String)cmbSelectReport2.getValue());
+		report.load_dir(directory);
+		Scene serverScene = new Scene(root);
+		serverScene.getStylesheets().add(getClass().getResource("report_order.css").toExternalForm());
+		primaryStage.setScene(serverScene);
+		primaryStage.show();
+	}
 	@FXML
 	public void LogoutBtn(ActionEvent event)
 	{
